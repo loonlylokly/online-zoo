8aeadb13a46aa2e2cff89eeb2700e8e298414742
loonlylokly <krivdinm@ya.ru>
2022-10-13 21:26:35 +0300

    fix: popup, amount

--- online-zoo/scripts/donate.js
+++ online-zoo/scripts/donate.js
@@ -11,21 +11,27 @@ const inputAmount = document.querySelector(".input-wrap__amount");
 inputAmount.addEventListener("input", (e) => {
     findValue(e.target.value, donateDots);
     if (e.target.value.length >= 5)
-      inputAmount.value = inputAmount.value.slice(0, 4);
-  });
+        inputAmount.value = inputAmount.value.slice(0, 4);
+});
 
 
+const findValue = (value, dots) => {
+    const currentItem = [...dots].find((item) => item.value === value);
+    toggleActive(currentItem, dots);
+};
 
-  const toggleActive = (amount, dots) => {
+
+const toggleActive = (amount, dots) => {
     dots.forEach(() => {
-      const activeDot = document.querySelector(".active-dot");
-      if (activeDot) activeDot.classList.remove("active-dot");
+        const activeDot = document.querySelector(".active-dot");
+        if (activeDot) activeDot.classList.remove("active-dot");
     });
+
     if (amount) {
-      amount.children[0].classList.add("active-dot");
-      inputAmount.value = +amount.value;
+        amount.children[0].classList.add("active-dot");
+        inputAmount.value = +amount.value;
     }
-  };
+};
 
 
 
